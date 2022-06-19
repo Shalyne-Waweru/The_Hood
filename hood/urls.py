ba07b6251392eac2d203e59714cd293e8cc456cb
Shalyne-Waweru <shalynewaweru@gmail.com>
2022-06-19 21:06:43 +0300

    Add and style the signup page

--- hood/urls.py
+++ hood/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('join_hood/', views.join_hood, name='join-hood'),
     path('profile/', views.profile, name='profile'),
     path('login/', views.login_user, name='login'),
+    path('signup/', views.signup, name='signup'),
 ]
--- hood/views.py
+++ hood/views.py
@@ -30,3 +30,20 @@ def login_user(request):
     View function that renders the login page and its data
     '''
     return render(request, 'auth/login.html')
+
+def signup(request):
+    '''
+    View function that renders the signup page and its data
+    '''
+    # if request.method == 'POST':
+    #     form = SignupForm(request.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #         username = form.cleaned_data.get('username')
+    #         password = form.cleaned_data.get('password1')
+    #         user = authenticate(username=username, password=password)
+    #         login(request, user)
+    #         return redirect('index')
+    # else:
+    #     form = SignupForm()
+    return render(request, 'auth/signup.html', locals())
