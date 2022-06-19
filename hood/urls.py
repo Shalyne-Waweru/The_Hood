6682fe0d1869f3946cdf3423e069db3ece38b2c8
Shalyne-Waweru <shalynewaweru@gmail.com>
2022-06-19 20:42:14 +0300

    Add and style the neighborhood info page

--- hood/urls.py
+++ hood/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'indexPage'),  
+    path('join_hood/', views.join_hood, name='join-hood'),
 ]
--- hood/views.py
+++ hood/views.py
@@ -6,3 +6,14 @@ def index (request):
     View function that returns the landing page and its data
     '''   
     return render(request, 'index.html')
+
+# @login_required(login_url='login')
+def join_hood(request):
+    '''
+    View function that renders the neighborhood page and its data
+    '''
+    # neighbourhood =get_object_or_404(neighbourhood,id=id)
+    # request.user.profile.neighbourhood =neighbourhood
+    # request.user.profile.save()
+    # return redirect('hood')
+    return render(request, 'single_hood.html')
